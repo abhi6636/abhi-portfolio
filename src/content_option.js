64b7c9612cfcb3410ff8757e72b6c8e46459c49b
Abhinand <abhinandm94@gmail.com>
2023-04-06 15:00:03 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,152 @@
+const logotext = "ABHINAND";
+const meta = {
+    title: "Abhinand",
+    description: "I’m Abhinand Web Developer _ Currently studying MERN Stack",
+};
+
+const introdata = {
+    title: "I’m Abhinand",
+    animated: {
+        first: "I love coding 🥰",
+        second: "I develop responsive websites 😎",
+        third: "I use tabs over spaces 😉",
+    },
+    description: `Hi, I'm Abhinand. My passion for coding and problem-solving has led me to specialize in MERN stack Development.
+    Throughout my career, I've had the opportunity to work on a wide variety of projects, ranging from simple websites to complex web applications.  `,
+    your_img_url: "https://github.com/abhi6636/abhi6636/blob/main/abi.png?raw=true",
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: "Accomplished web developer in designing and maintaining websites. Recognized for leadership and collaborative abilities when working as team. Motivated to provide clients with user-friendly applications.",
+};
+const worktimeline = [
+    {
+        jobtitle: "MERN Stack Trainee",
+        where: "Bridgeon",
+        date: "2023",
+    },
+    {
+        jobtitle: "Content Moerator",
+        where: "Freelance",
+        date: "2021",
+    },
+];
+
+const skills = [
+    {
+        name: "HTML",
+        value: 80,
+    },
+    {
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "CSS",
+        value: 70,
+    },
+    {
+        name: "React",
+        value: 35,
+    },
+];
+
+const services = [{
+        title: "Web Development",
+        description: "Professional design and implementation of websites, web Apps and other solutions.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b680a062246147.5a8a773c6932d.gif",
+        description: "Rock Paper Scissors mini Game",
+        link: "https://github.com/abhi6636/bridgeon/tree/main/rock-paper-scissors",
+    },
+    {
+        img: "https://images.barrons.com/im-338004?width=548&height=421",
+        description: "Banking site using simple HTML codes",
+        link: "https://github.com/abhi6636/bank-of-kunnamkulam.git",
+    },
+    {
+        img: "https://images.ctfassets.net/e8i6c2002cqg/2hce1peyK2Ah0k9PBpDPMZ/1823e49bb97709c2b6a83d472878237d/Reveal_Animation.gif",
+        description: "Wellfound.com Clone site using simple HTML.",
+        link: "https://github.com/abhi6636/angelco-clone.git",
+    },
+    {
+        img: "https://cdn.dribbble.com/users/1042835/screenshots/8163263/media/040e55528ea1fdc16ee9ef4d1688761d.gif",
+        description: "Simple Web App have login and signup, dashboard for user fatures and can store datas in firebase database and perform CRUD Operations using React",
+        link: "https://github.com/abhi6636/badhaan-test.git",
+    },
+    {
+        img: "https://cdn.dribbble.com/users/125056/screenshots/9166068/media/0479b706f1692768d5f185d174292b24.gif",
+        description: "Login and Infinite Scroll list web app using React",
+        link: "https://github.com/abhi6636/infinite-scroll-list.git",
+    },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "abhinandm94@gmail.com",
+    YOUR_FONE: "+91 9895101038",
+    description: "For any type of online project please don't hesitate to get in touch with me. The fastest way is Shoot me an Email. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_i2c5e2c",
+    YOUR_TEMPLATE_ID: "template_y8q9olm",
+    YOUR_USER_ID: "JmuunVn02H5LG0Mjd",
+};
+
+const socialprofils = {
+    github: "https://github.com/abhi6636",
+    facebook: "https://facebook.com/abhienandh",
+    linkedin: "https://linkedin.com/abhinand-m-s-712004168",
+    twitter: "https://twitter.com/mnuOffl",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
